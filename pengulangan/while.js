d694bd26780a789ec41adb6c790c8ecaedcc4795
Fadlyyyy <fadlyynasah86@f=gmail.com>
2022-12-19 21:38:27 +0700

    pengulangan

---
+++ pengulangan/while.js
@@ -0,0 +1,19 @@
+// console.log('Hello World');
+
+// var ulang = true;
+// while (ulang) {
+//     console.log('Hello World');
+//     ulang = confirm('lagi?');
+// }
+
+//  looping dihentikan oleh diri developer
+
+var nilaiAwal = 1;
+while(nilaiAwal <= 10) {
+    console.log('Hello World ' + nilaiAwal + 'x');
+nilaiAwal++;
+}
+
+
+// increment penambahan pada nilai awal
+// decrement pengurangan pada nilai awal
